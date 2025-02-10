f8a01ab765473d7980717ad5910218e46e84d432
https://github.com/YafreisiPerez/YafreisiP.github.io.git <yafreisiperez03@gmail.com>
2025-02-09 21:17:44 -0400

    MI COMMIT CON INTEGRACION     CONTINUA

---
+++ hello.js
@@ -0,0 +1,8 @@
+function Helloworld() {
+    return "¡Hola Mundo!";
+  }
+  
+  module.exports = Helloworld;
+  
+
+  //Yafreisi Perez 2020-9871
---
+++ hello.test.js
@@ -0,0 +1,7 @@
+const hellowold = requiere("./hello");
+
+TextDecoderStream("Debe retornar'Hola Mundo'", () => {
+    expect(helloworld()).toBe("Hola Mundo");
+});
+
+//Yafreisi Perez 2020-9871
